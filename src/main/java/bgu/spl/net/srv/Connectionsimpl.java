dd2785866722f6a0e133450c9772856a054be6b2
Danielle Duek <Daniellush20060@gmail.com>
2020-01-08 22:45:25 +0200

    connectionsimpl

--- src/main/java/bgu/spl/net/srv/Connectionsimpl.java
+++ src/main/java/bgu/spl/net/srv/Connectionsimpl.java
@@ -1,18 +1,45 @@
 package bgu.spl.net.srv;
 
-public class Connectionsimpl implements Connections {
+import java.util.HashMap;
+import java.util.concurrent.BlockingQueue;
+
+public class Connectionsimpl<T> implements Connections<T> {
+
+    private final HashMap<Integer, ConnectionHandler<T>> clients = new HashMap<>();
+    private HashMap<String, BlockingQueue<Integer>> genres = new HashMap<>();
+    private Object Integer;
+    //  private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
     @Override
     public boolean send(int connectionId, Object msg) {
-        return false;
-    }
+    //    try {
+          //  lock.readLock().lock();
+            ConnectionHandler<T> handler = clients.get(connectionId);
+           // lock.readLock().unlock();
+            handler.send((T) msg);
+            return true;
+        }
+      //  catch (Exception e) {
+         //   return false;
+     //   }
 
     @Override
-    public void send(String channel, Object msg) {
-
+    public void send(String genre, Object msg) {
+       //  lock.readLock().lock();
+        if(genres.containsKey(genre)){
+            for(int i=0; i<genres.get(genre).size(); i++){
+                ConnectionHandler<T> handler = clients.get(i);
+                handler.send((T)msg);
+            }
+        }
+      //  lock.readLock().unlock();
     }
 
     @Override
     public void disconnect(int connectionId) {
-
+        //try {
+    //    lock.writeLock().lock();
+        clients.remove(connectionId);
+    //    lock.writeLock().unlock();
     }
 }
