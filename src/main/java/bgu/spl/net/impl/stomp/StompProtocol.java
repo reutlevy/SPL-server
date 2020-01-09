907c7326c465e3226479edda49aff9f0e93b5c40
reut <reutlevy30@gmail.com>
2020-01-09 21:23:54 +0200

    Merge branch 'master' of https://github.com/reutlevy/SPL-server
    # Conflicts:
    #	src/main/java/bgu/spl/net/impl/stomp/StompFrame.java

--- src/main/java/bgu/spl/net/impl/stomp/StompProtocol.java
+++ src/main/java/bgu/spl/net/impl/stomp/StompProtocol.java
@@ -3,26 +3,60 @@ package bgu.spl.net.impl.stomp;
 import bgu.spl.net.api.StompMessagingProtocol;
 import bgu.spl.net.srv.Connections;
 import bgu.spl.net.srv.Connectionsimpl;
-import bgu.spl.*;
+import bgu.spl.net.srv.DataBase;
 
 import java.util.HashMap;
 
 public class StompProtocol implements StompMessagingProtocol<StompFrame> {
 
     private Boolean terminate=false;
-    private Connectionsimpl<String> connections;
+    private Connectionsimpl<StompFrame> connections;
     private int id;
     private StompFrame response;
     private HashMap<String,String> message;
+    private DataBase db;
 
-    public void start(int connectionId, Connections<String> connections){
+    @Override
+    public void start(int connectionId, Connections<StompFrame> connections){
       this.id=connectionId;
-      this.connections= (Connectionsimpl<String>)connections;
+      this.connections= (Connectionsimpl<StompFrame>)connections;
       message=new HashMap<String, String>();
+      db=DataBase.getInstance();
+      db.initialConnections((Connectionsimpl<StompFrame>) connections);
     }
 
+    @Override
     public void process(StompFrame frame){
         String type=frame.getType();
+        HashMap<String,String> add=frame.getHashMap();
+        if(type.equals("DISCONNECT")){
+            terminate=true;
+            message.put("receipt-id",add.get("receipt"));
+            response=new RECEIPT("RECEIPT",message);
+            connections.send(id,response);
+        }
+        if(type.equals("CONNECT")){
+            message.put("version",add.get("accept-version"));
+            response=new CONNECTED("CONNECTED",message);
+            connections.send(id,response);
+        }
+        if(type.equals("SUBSCRIBE")){
+            message.put("receipt-id",add.get("receipt"));
+            response=new RECEIPT("RECEIPT",message);
+            connections.send(id,response);
+        }
+        if((type.equals("MESSAGE")||(type.equals("SEND")))){
+            message.put("subscription",String.valueOf(id));
+            message.put("destination",add.get("destination"));
+            message.put("body",add.get("body"));
+            response=new MESSAGE("MESSAGE",message);
+            connections.send(id,response);
+        }
+        if(frame.getIsError()){
+            message.put("receipt-id",add.get("receipt"));
+            response=new ERROR("RECEIPT",message);
+            connections.send(id,response);
+        }
     }
 
     public boolean shouldTerminate(){
